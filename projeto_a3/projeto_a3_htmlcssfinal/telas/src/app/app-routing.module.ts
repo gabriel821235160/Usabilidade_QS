dc180c513bfcf9454018ead9764ffc2e60fc4477
limar <you@example.com>
2022-11-11 09:31:44 -0300

    11.11

--- projeto_a3/projeto_a3_htmlcssfinal/telas/src/app/app-routing.module.ts
+++ projeto_a3/projeto_a3_htmlcssfinal/telas/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './views/cadastro/cadastro.component';
 import { LoginComponent } from './views/login/login.component';
+import { InicialComponent } from './views/inicial/inicial.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
+  {path: '', component: InicialComponent},
+  {path: 'login', component: LoginComponent},
   {path:'cadastro', component: CadastroComponent}
 ];
 
