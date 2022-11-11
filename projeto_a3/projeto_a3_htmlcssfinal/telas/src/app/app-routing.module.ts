cfab03895a90d3afea75689ebd65b485eea141f4
limar <you@example.com>
2022-11-11 09:11:42 -0300

    11.11

---
+++ projeto_a3/projeto_a3_htmlcssfinal/telas/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { LoginComponent } from './views/login/login.component';
+import { InicialComponent } from './views/inicial/inicial.component';
+
+const routes: Routes = [
+  {path: '', component: InicialComponent},
+  {path:'login', component: LoginComponent},
+  {path:'cadastro', component: CadastroComponent} 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ projeto_a3/projeto_a3_htmlcssfinal/telas/src/app/views/login/login.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../../../../../autenticacao/models/usuario';
+//'src/app/model/medico/medico';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  cpf=''
+  senha=''
+
+ constructor(private HttpClient: HttpClient) { }
+  
+ 
+ connectionLogin(){
+    return this.HttpClient.post('http://localhost:7000/logar',
+    {cpf: this.cpf, senha: this.senha})
+    .pipe(
+      (res) => {
+        console.log ("retorno", res)
+        return res
+      },
+      (err) =>{
+        console.log ("erro", err)
+        return err
+      } 
+    )
+  }
+  
+  jose(){
+    console.log("estou por aqui")
+  }
+
+  ngOnInit(): void {
+  }
+
+}
